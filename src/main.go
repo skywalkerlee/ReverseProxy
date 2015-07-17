5d69ba7f79a33f721ac1efcd1b66f99a6fe8e63f
李哲源 <lzy460332595@gmail.com>
2015-07-17 11:02:33 +0800

    Update main.go

--- src/main.go
+++ src/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	proxy := httputil.NewSingleHostReverseProxy(&url.URL{Scheme: "http", Host: "127.0.0.1:9001", Path: "/"})
-	err := http.ListenAndServe(":443",  proxy)
+	err := http.ListenAndServe(":80",  proxy)
 	if err != nil {
 		log.Fatalln(err)
 	}
